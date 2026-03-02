6a0476267371d8797ffb7c90de8feee4488e6505
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2026-03-02 20:25:52 +0100

    tutorial 18. done

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,25 +1,49 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls _controls;
+    private CharacterController _characterController;
 
-    public Vector2 moveInput;
-    public Vector2 aimInput;
+    [Header("Movement info")]
+    [SerializeField] private float walkSpeed;
+    private Vector3 _movementDirection;
+    
+    private Vector2 _moveInput;
+    private Vector2 _aimInput;
 
     private void Awake()
     {
         _controls = new PlayerControls();
         
-        _controls.Character.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        _controls.Character.Movement.canceled += _ => moveInput = Vector2.zero;
+        _controls.Character.Movement.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
+        _controls.Character.Movement.canceled += _ => _moveInput = Vector2.zero;
         
-        _controls.Character.Aim.performed += ctx => aimInput = ctx.ReadValue<Vector2>();
-        _controls.Character.Aim.canceled += _ => aimInput = Vector2.zero;
+        _controls.Character.Aim.performed += ctx => _aimInput = ctx.ReadValue<Vector2>();
+        _controls.Character.Aim.canceled += _ => _aimInput = Vector2.zero;
     }
-    
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        _movementDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
+
+        if (_movementDirection.magnitude > 0)
+        {
+            _characterController.Move(_movementDirection * (Time.deltaTime * walkSpeed));
+        }
+    }
+
     private void OnEnable()
     {
         _controls.Enable();
