5349ce935a99c72409872e4013774ac3c54c19f9
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2026-03-02 21:57:34 +0100

    tutorial 18. done

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private const float Gravity = 9.81f;
+    
     private PlayerControls _controls;
     private CharacterController _characterController;
 
     [Header("Movement info")]
     [SerializeField] private float walkSpeed;
     private Vector3 _movementDirection;
+    
+    private float _verticalVelocity;
     
     private Vector2 _moveInput;
     private Vector2 _aimInput;
@@ -37,10 +40,24 @@ public class PlayerMovement : MonoBehaviour
     private void ApplyMovement()
     {
         _movementDirection = new Vector3(_moveInput.x, 0, _moveInput.y);
+        ApplyGravity();
 
         if (_movementDirection.magnitude > 0)
         {
             _characterController.Move(_movementDirection * (Time.deltaTime * walkSpeed));
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (!_characterController.isGrounded)
+        {
+            _verticalVelocity -= Gravity * Time.deltaTime;
+            _movementDirection.y = _verticalVelocity;
+        }
+        else
+        {
+            _verticalVelocity = -0.5f;
         }
     }
 
