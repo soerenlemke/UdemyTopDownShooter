406ed99cd3c28b5b16ad7e0b08208fb170f0471f
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2026-03-04 12:25:25 +0100

    tutorial 25. done

---
+++ Assets/WeaponVisualController.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+public class WeaponVisualController : MonoBehaviour
+{
+    [SerializeField] private Transform[] gunTransforms;
+    
+    [SerializeField]  private Transform pistol;
+    [SerializeField]  private Transform revolver;
+    [SerializeField]  private Transform autoRifle;
+    [SerializeField]  private Transform shotgun;
+    [SerializeField]  private Transform rifle;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SwitchOn(pistol);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SwitchOn(revolver);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            SwitchOn(autoRifle);
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            SwitchOn(shotgun);
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            SwitchOn(autoRifle);
+    }
+
+    private void SwitchOn(Transform gunTransform)
+    {
+        SwitchOffGuns();
+        gunTransform.gameObject.SetActive(true);
+    }
+
+    private void SwitchOffGuns()
+    {
+        foreach (var gunTransform in gunTransforms)
+        {
+            gunTransform.gameObject.SetActive(false);
+        }
+    }
+}
