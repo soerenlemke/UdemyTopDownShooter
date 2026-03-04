8c9d96660ef9857a80cc58de1ff2c71845813b72
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2026-03-04 19:10:48 +0100

    tutorial 30. done

--- Assets/WeaponVisualController.cs
+++ Assets/WeaponVisualController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class WeaponVisualController : MonoBehaviour
 {
+    private Animator _animator;
+    
     [SerializeField] private Transform[] gunTransforms;
     
     [SerializeField]  private Transform pistol;
@@ -11,24 +13,58 @@ public class WeaponVisualController : MonoBehaviour
     [SerializeField]  private Transform shotgun;
     [SerializeField]  private Transform rifle;
 
+    private Transform _currentGun;
+    
+    [Header("Left hand IK")]
+    [SerializeField] private Transform leftHand;
+
+    private void Start()
+    {
+        SwitchOn(pistol);
+        
+        _animator = GetComponentInParent<Animator>();
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
             SwitchOn(pistol);
+            SwitchAnimationLayer(1);
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
             SwitchOn(revolver);
+            SwitchAnimationLayer(1);
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
             SwitchOn(autoRifle);
+            SwitchAnimationLayer(1);
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
             SwitchOn(shotgun);
+            SwitchAnimationLayer(2);
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
             SwitchOn(rifle);
+            SwitchAnimationLayer(3);
+        }
     }
 
     private void SwitchOn(Transform gunTransform)
     {
         SwitchOffGuns();
         gunTransform.gameObject.SetActive(true);
+        _currentGun = gunTransform;
+        
+        AttachLeftHand();
     }
 
     private void SwitchOffGuns()
@@ -37,5 +73,22 @@ public class WeaponVisualController : MonoBehaviour
         {
             gunTransform.gameObject.SetActive(false);
         }
+    }
+
+    private void AttachLeftHand()
+    {
+        var targetTransform = _currentGun.GetComponentInChildren<LeftHandTargetTransform>().transform;
+        leftHand.localPosition = targetTransform.localPosition;
+        leftHand.localRotation = targetTransform.localRotation;
+    }
+
+    private void SwitchAnimationLayer(int layerIndex)
+    {
+        for (var i = 1; i < _animator.layerCount; i++)
+        {
+            _animator.SetLayerWeight(i, 0);
+        }
+        
+        _animator.SetLayerWeight(layerIndex, 1);
     }
 }
